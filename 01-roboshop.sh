edc97257c2a53346381e2c7f6bb3c0a14d96ca59
balaji <balubongu185@gmail.com>
2026-02-04 00:30:59 +0530

    roboshop

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -4,12 +4,14 @@ sgroup="sg-0045e5825324c775b"
 
 for Instan in $@
 do
-    instance_id=$(aws ec2 run-instances 
-    --image-id ami-0220d79f3f480ecf5 
-    --count 1 --instance-type t3.micro 
-    --security-group-ids sg-0045e5825324c775b --tag-specifications 
-    'ResourceType=instance,Tags=[{Key=Name,Value=$Instan}]'
-    --output text)
+    instance_id=$(aws ec2 run-instances \
+  --image-id ami-0220d79f3f480ecf5 \
+  --count 1 \
+  --instance-type t3.micro \
+  --security-group-ids sg-0045e5825324c775b \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Instan}]" \
+  --output text
+    )
     if [ $instance =! "frontend"]; then
         ip=$(aws ec2 describe-instances \
     --instance-ids i-01292ce7d88fc5494 \
