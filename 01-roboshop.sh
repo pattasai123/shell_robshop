039edec435aa5f313f47dd07e185c8df8f4b30b4
balaji <balubongu185@gmail.com>
2026-02-04 00:46:28 +0530

    roboshop

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -5,24 +5,27 @@ sgroup="sg-0045e5825324c775b"
 for Instan in $@
 do
     instance_id=$(aws ec2 run-instances \
-  --image-id $ami_id \
-  --count 1 \
-  --instance-type t3.micro \
-  --security-group-ids $sgroup \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Instan}]" \
-  --output text
+      --image-id $ami_id \
+      --count 1 \
+      --instance-type t3.micro \
+      --security-group-ids $sgroup \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Instan}]" \
+      --query 'Instances[0].InstanceId' \
+      --output text
     )
-    if [ $Instan =! "frontend"]; then
+
+    if [ "$Instan" != "frontend" ]; then
         ip=$(aws ec2 describe-instances \
-    --instance-ids $instance_id \
-    --query 'Reservations[0].Instances[0].PublicIpAddress' \
-    --output text)
+          --instance-ids $instance_id \
+          --query 'Reservations[0].Instances[0].PublicIpAddress' \
+          --output text)
     else
-       ip=$(aws ec2 describe-instances \
-    --instance-ids $instance_id \
-    --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-    --output text)
+        ip=$(aws ec2 describe-instances \
+          --instance-ids $instance_id \
+          --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+          --output text)
     fi
-    echo "instance is created and public or prive ip is captured"
+
+    echo "Instance $Instan created with IP: $ip"
 
 done
