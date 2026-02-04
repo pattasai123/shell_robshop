565bde92ef93c42e1bd45203ce97dd9093d9e9e2
balaji <balubongu185@gmail.com>
2026-02-04 12:54:28 +0530

    installation

---
+++ mongodb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+user= $(id -u)
+if [ $user -ne 0 ]; then 
+    echo "please run this with root access"
+    exit 1
+fi
+r="\e[32m"
+g="\e[33m"
+folder="/var/log/shell_roboshop"
+file=$(echo $0 | cur -d "." -f1)
+filename="$folder/$file.log"
+mkdir -p $folder
+validate(){
+    if [ $1 -eq 0 ]; then
+        echo -e "$2...$r success $g" | tee -a $filename
+    else 
+        echo -e "$2 ... $r Failure $g " | tee -a $filename
+    fi
+}
+
+cp mongo.repo vim /etc/yum.repos.d/mongo.repo
+dnf install mongodb-org -y &>> $filename
+validate $? "Installing mongodb"
+systemctl enable mongod &>> $filename
+validate $? "Enableing mongodb"
+systemctl start mongod  &>> $filename
+validate $? "Starting mongodb"
