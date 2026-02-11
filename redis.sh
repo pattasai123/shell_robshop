80288edd8b48490ac020f2545dbf37c0b3fea3a3
balaji <balubongu185@gmail.com>
2026-02-11 17:02:45 +0530

    catalogue

--- redis.sh
+++ redis.sh
@@ -28,7 +28,7 @@ dnf module enable redis:7 -y &>> $filename
 validate $? "enable redis"
 dnf install redis -y &>> $filename
 validate $? "Installing redis"
-sed -i -e "s/127.0.0.0/0.0.0.0/" -e "/protected-mode/c protected-mode no" /etc/redis/redis.conf
+sed -i -e "s/127.0.0.1/0.0.0.0/g" -e "/protected-mode/ c protected-mode no" /etc/redis/redis.conf
 systemctl enable redis &>> $filename
 validate $? "Enableing redis"
 systemctl start redis  &>> $filename
